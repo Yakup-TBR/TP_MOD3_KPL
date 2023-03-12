c4c871c8900498fb4f46eab4deaab13ad1fccb37
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-03-13 00:47:52 +0800

    menambahkan implementasi dengan state-based construction

--- tpmodul3_1302210113/Program.cs
+++ tpmodul3_1302210113/Program.cs
@@ -4,7 +4,7 @@ using System;
 
 public class KodePos
 {
-    public string getKodePos(string kelurahan)
+    public string getKodePos_1302210113(string kelurahan)
     {
         switch (kelurahan)
         {
@@ -33,27 +33,84 @@ public class KodePos
     }
 } 
 
+class DoorMachine
+{
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = new LockedState();
+    }
+
+    public void SetState(State state)
+    {
+        currentState = state;
+        currentState.DisplayState();
+    }
+}
+
+interface State
+{
+    void DisplayState();
+}
+
+class LockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu terkunci");
+    }
+}
+
+class UnlockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+}
+
+
+
+
+
+
 public class Program
 {
     static void Main()
     {
+        //Bagian Tabel Keluarahan KodePos
         KodePos kodePos = new KodePos();
 
         Console.WriteLine("| Kelurahan\t\t| Kode Pos |");
         Console.WriteLine("--------------------------------------");
 
-        Console.WriteLine($"| Batununggal\t\t| {kodePos.getKodePos("Batununggal")}\t|");
-        Console.WriteLine($"| Kujangsari\t\t| {kodePos.getKodePos("Kujangsari")}\t|");
-        Console.WriteLine($"| Mengger\t\t| {kodePos.getKodePos("Mengger")}\t|");
-        Console.WriteLine($"| Wates\t\t\t| {kodePos.getKodePos("Wates")}\t|");
-        Console.WriteLine($"| Cijaura\t\t| {kodePos.getKodePos("Cijaura")}\t|");
-        Console.WriteLine($"| Jatisari\t\t| {kodePos.getKodePos("Jatisari")}\t|");
-        Console.WriteLine($"| Margasari\t\t| {kodePos.getKodePos("Margasari")}\t|");
-        Console.WriteLine($"| Sekejati\t\t| {kodePos.getKodePos("Sekejati")}\t|");
-        Console.WriteLine($"| Kebonwaru\t\t| {kodePos.getKodePos("Kebonwaru")}\t|");
-        Console.WriteLine($"| Maleer\t\t| {kodePos.getKodePos("Maleer")}\t|");
-        Console.WriteLine($"| Samoja\t\t| {kodePos.getKodePos("Samoja")}\t|");
+        Console.WriteLine($"| Batununggal\t\t| {kodePos.getKodePos_1302210113("Batununggal")}\t|");
+        Console.WriteLine($"| Kujangsari\t\t| {kodePos.getKodePos_1302210113("Kujangsari")}\t|");
+        Console.WriteLine($"| Mengger\t\t| {kodePos.getKodePos_1302210113("Mengger")}\t|");
+        Console.WriteLine($"| Wates\t\t\t| {kodePos.getKodePos_1302210113("Wates")}\t|");
+        Console.WriteLine($"| Cijaura\t\t| {kodePos.getKodePos_1302210113("Cijaura")}\t|");
+        Console.WriteLine($"| Jatisari\t\t| {kodePos.getKodePos_1302210113("Jatisari")}\t|");
+        Console.WriteLine($"| Margasari\t\t| {kodePos.getKodePos_1302210113("Margasari")}\t|");
+        Console.WriteLine($"| Sekejati\t\t| {kodePos.getKodePos_1302210113("Sekejati")}\t|");
+        Console.WriteLine($"| Kebonwaru\t\t| {kodePos.getKodePos_1302210113("Kebonwaru")}\t|");
+        Console.WriteLine($"| Maleer\t\t| {kodePos.getKodePos_1302210113("Maleer")}\t|");
+        Console.WriteLine($"| Samoja\t\t| {kodePos.getKodePos_1302210113("Samoja")}\t|");
 
         Console.WriteLine("--------------------------------------");
+
+
+
+        Console.WriteLine();
+
+        //Bagian DoorMachine
+
+        DoorMachine doorMachine = new DoorMachine();
+
+        // Mengubah state menjadi UnlockedState
+        doorMachine.SetState(new UnlockedState());
+
+        // Mengubah state menjadi LockedState
+        doorMachine.SetState(new LockedState());
     }
 }
